867eb7fe2a56565bcd43dc1dd5e2d2165635a8dc
Sandra Krogh <Sandrask85@hotmail.com>
2021-05-21 09:44:40 +0200

    D. 21 maj

---
+++ LysApp/Header/zeroCross.h
@@ -0,0 +1,16 @@
+/*
+ * zeroCross.h
+ *
+ * Created: 21-05-2021 08:55:34
+ *  Author: sandr
+ */ 
+
+
+#ifndef ZEROCROSS_H_
+#define ZEROCROSS_H_
+
+
+
+
+
+#endif /* ZEROCROSS_H_ */
---
+++ LysApp/Source/zeroCross.c
@@ -0,0 +1,6 @@
+/*
+ * zeroCross.c
+ *
+ * Created: 21-05-2021 08:55:19
+ *  Author: sandr
+ */ 
